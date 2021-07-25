14a938047448e201c240b0f0988a8efe0ef20ff3
nasonovandrey <andrey.m.nasonov@gmail.com>
2021-07-25 10:19:33 +0300

    redirected subprocess to pty for real-time output

--- handler.py
+++ handler.py
@@ -1,4 +1,6 @@
 import subprocess
+from select import select
+import errno
 import string
 import sys
 import os
@@ -16,12 +18,11 @@ if __name__ == '__main__':
 
     out_r, out_w = pty.openpty()
     err_r, err_w = pty.openpty()
-    outfile = open(CONTROL_DIR+'/'+uid+'/'+'outfile', 'wb+')
-    errfile = open(CONTROL_DIR+'/'+uid+'/'+'errfile', 'wb+')
     profile = open(CONTROL_DIR+'/'+uid+'/'+'profile', 'w+')
 
     profile.write(str(datetime.now())+'\n')
     profile.flush()
+
 
     proc = subprocess.Popen(sys.argv[2:], stdout=out_w, stderr=err_w)
     os.close(out_w)
@@ -31,17 +32,28 @@ if __name__ == '__main__':
     profile.write(str(pid)+'\n')
     profile.flush()
 
-    while proc.poll() is None:
-        outline = os.read(out_r, 1000)
-        errline = os.read(err_r, 1000)
-        outfile.write(outline)
-        outfile.flush()
-        errfile.write(errline)
-        errfile.flush()
+    outfile = open(CONTROL_DIR+'/'+uid+'/'+'outfile', 'wb+')
+    errfile = open(CONTROL_DIR+'/'+uid+'/'+'errfile', 'wb+')
+
+    streams = {out_r:outfile, err_r:errfile}
+
+    while streams:
+        ready_fds, _, _ = select(list(streams.keys()), [], [])
+        for fd in ready_fds:
+            try:
+                output = os.read(fd, 1)
+                streams[fd].write(output)
+                streams[fd].flush()
+            except OSError as e:
+                if e.errno != errno.EIO:
+                    raise
+                output = ''
+            if not output:
+                streams.pop(fd)
+
         
     profile.write(str(datetime.now())+'\n')
     profile.write(str(proc.returncode)+'\n')
     profile.write('DONE\n')
     profile.close()
-    
-    
+ 
